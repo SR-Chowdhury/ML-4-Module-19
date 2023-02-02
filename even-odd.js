2ca60b3e9977e01299dc999334967302ae03f0ee
Shihan Chowdhury <hackedgdown@gmail.com>
2023-02-02 20:33:35 +0000

    first commit

---
+++ even-odd.js
@@ -0,0 +1,16 @@
+var x = 0;
+var y = 1;
+
+// Find the Even Number
+console.log('Even Number: \n');
+while(x <= 50) {
+    console.log(x);
+    x+=2;
+}
+
+// Find the Odd Number
+console.log('Odd Number: \n');
+while(y <= 50) {
+    console.log(y);
+    y+=2;
+}
